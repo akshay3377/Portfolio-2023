de00c4d899d32f7585e836f9df6ad100c68ed889
akshay3377 <workwithakshay777@gmail.com>
2023-08-22 23:34:01 +0530

    added lazy loading with suspense

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import HomeSection from "@/components/features/home";
+// import HomeSection from "@/components/features/home";
+import React, { lazy, Suspense } from "react";
+const HomeSection = lazy(() => import("@/components/features/home"));
 
 export default function Home() {
   return (
-    <>
-      <HomeSection />
-    </>
+    <div>
+      <Suspense fallback={<div className="bg-red">Loading.........</div>}>
+        <HomeSection />
+      </Suspense>
+    </div>
   );
 }
+
+// import React, { lazy, Suspense } from 'react';
+
+// const LazyComponent = lazy(() => import('./LazyComponent'));
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>React Dynamic Import Example</h1>
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <LazyComponent />
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// export default App;
