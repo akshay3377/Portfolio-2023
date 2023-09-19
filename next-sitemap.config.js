f4750b0ce7e160bb2c8dc25772db39830564c4ef
akshay3377 <workwithakshay777@gmail.com>
2023-09-19 16:55:24 +0530

    added dynamic sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+// next-sitemap.js
+
+module.exports = {
+    siteUrl: 'http://localhost:3000/', // Your website URL
+    generateRobotsTxt: true, // Generate robots.txt file
+    // exclude: ['/admin'], // Paths to exclude from the sitemap
+    // changefreq: 'daily', // Set the change frequency of URLs
+    // priority: 0.7, // Set the priority of URLs
+    // sitemapSize: 5000, // Set the maximum number of URLs per sitemap file
+    sitemapName: 'sitemap.xml', // Set the sitemap file name
+  };
+  
